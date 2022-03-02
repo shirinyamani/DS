e56dbca096e7a0a007d184cf0af112535ca1ce47
shirin <75791599+shirin1996@users.noreply.github.com>
2022-03-02 21:46:00 +0330

    longest qz added

--- Array/array-leetcode.py
+++ Array/array-leetcode.py
@@ -1,8 +1,12 @@
 import collections
 import enum
 from palindrome import Solution
+
 #sum of three numbers equal to zero 
-def threeSum(num):
+#LESSON1: define an empty set/list when ya wanna build a new
+#LESSON2: use a set to store the numbers
+#LESSON3: use list(i) to convert a set to a list
+def threeSum1(num):
     num.sort()
     set_num= set()
     for i in range(len(num)):
@@ -19,17 +23,23 @@ def threeSum(num):
                 r-=1
     return [list(i) for i in set_num]
 
-#find anaagrams
-def findAnagram(arr):
+def groupAnagrams(words):
+        anagram = collections.defaultdict(list)
+        for word in words:
+            key = "".join(sorted(word))
+            anagram[key].append(word)
+        return anagram.values()
+            
+def groupAnagram1(arr):
     anagram ={}
     for word in arr:
         sorted_word = "".join(sorted(word))
         if word in anagram:
             anagram[sorted_word].append(word)
-        else: anagram[sorted_word]= [word]
     return anagram.values()
 
 #find the longest substring of a list of strings
+#LESSON1:when ya wan the longest len, ya can either compare with 0 or use Max
 def longestsubstring(s):
     longest=""
     top=0
@@ -43,15 +53,15 @@ def longestsubstring(s):
     return top
 
 #only longest substring
-def longestSub(s):
+def longestSubstring2(s):
     longest=""
     len_longest=0
     for i in s:
-        longest += i
-        print(longest)
-        if len(longest)>len_longest:
-            len_longest=len(longest)
-    return longest
+        if i not in longest:
+            longest += i
+        else: longest = longest.split(i)[1] + i
+        len_longest = max(len_longest, len(longest))
+    return len_longest
 
 #find the longest palindrome in a string
 s = Solution()
@@ -96,5 +106,19 @@ def increasingTriplet(nums):
     return False
 
 
+def longestconsecutive(nums):
+    pass
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
-    print(increasingTriplet([0,-1,3,-50,20]))
+    #print(increasingTriplet([0,-1,3,-50,20]))
+    #print(threeSum1([1]))
+    #print(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+    print(longestSubstring2("abbbbcdddee"))
---
+++ Array/longestQ.py
@@ -0,0 +1,56 @@
+#Longest SubString
+def longestsubstring(words):
+    longest =""
+    n_longest=0
+    for chr in words:
+        if chr not in longest: #if the chr not in the longest str
+            longest += chr #add it
+        else: longest = longest.split(chr)[1]+chr #split the same chr in first position then concate the char to it
+        n_longest= max(n_longest, len(longest)) #het the max len
+    return n_longest
+
+#Longest SubSequence : 1)iterative 2)recurssive
+def longestSubsequence1(txt1,txt2):
+    dp = [[0 for i in range(len(txt2)+1)] for j in range(len(txt1)+1)]
+    for i in range(len(txt1) -1, -1,-1): #end i indice of the matrix
+        for j in range(len(txt2) -1,-1,-1): #end j indice of the matrix
+            if txt1[i] == txt2[j]: #they have same charactor
+                dp[i][j]= 1 + dp[i+1][j+1] #1 bc we have same chr in both
+            else: dp[i][j]= max(dp[i][j+1], dp[i+1][j])# go to nxt cell in i n j
+    return dp[0][0]
+
+#Longest Consecutive Sequence
+def longestConsecutive(nums):
+    longest_streak=0 #for comparison purpose
+    for num in nums:
+        current_num = num 
+        current_streak=1
+        
+        while current_num+1 in nums: #as long as nxt num is in the seq
+            current_num +=1 #go to nxt num
+            current_streak +=1 #extend the seq space
+            longest_streak = max(current_streak, longest_streak) 
+    return longest_streak
+
+#Longest Consecutive Sequence
+def longestConsecutive2(nums):
+    longest_streak = 0
+    set_num = set(nums)
+
+    for num in set_num:
+        if num-1 not in set_num: #take the current num in hand n check the prev
+            current_num = num
+            current_streak = 1
+
+            while current_num+1 in set_num: #if nxt num in the set then
+                current_num +=1 #go to next num 
+                current_streak +=1 #extend the streak
+
+                longest_streak= max(current_streak, longest_streak)
+    return longest_streak
+
+
+
+if __name__ =="__main__":
+    print(longestsubstring("abbxese"))
+    print(longestConsecutive2([0]))
