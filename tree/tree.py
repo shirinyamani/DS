7b497ca5b597dcca1b0f721b21a8a22eb1431a8a
shirin <75791599+shirin1996@users.noreply.github.com>
2022-04-12 19:00:54 +0430

    tree CTCI question 1+2 added

--- tree/tree.py
+++ tree/tree.py
@@ -25,3 +25,35 @@ class graph:
                         visited.append(adjnode) #if not path the mark it as visited
                         queue.append(adjnode)
         return path
+
+
+# Question 2
+class BSTNode:
+    def __init__(self, data=None, left = None, right= None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+def minimalTree(sortedArray, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    root = BSTNode(sortedArray[mid])
+    
+    root.left = minimalTree(sortedArray, start, mid-1)
+    root.right = minimalTree(sortedArray, mid+1, end)
+    return root 
+
+## OR
+
+def minimalTree1(sortedArray):
+    if len(sortedArray) == 0:
+        return None
+    if len(sortedArray) ==1:
+        return BSTNode(sortedArray[0])
+
+    mid = int(len(sortedArray/2))
+
+    left = minimalTree1(sortedArray[:mid])
+    right = minimalTree1(sortedArray[mid+1:])
+    return BSTNode(sortedArray[mid],left,right)
