e0b8d5d04f8d943090163a11ce09ee6f7bf5e26b
shirin <75791599+shirin1996@users.noreply.github.com>
2022-04-12 18:17:23 +0430

    tree CTCI question added

--- tree/tree.py
+++ tree/tree.py
@@ -1,1 +1,27 @@
+class graph:
+    def __init__(self,gdict):
+        if gdict is None:
+            gdict = {}
+        self.gdict = gdict
+
+    def addEdge(self,vertex, edge):
+        self.gdict[vertex].apped(edge)
+
 #Question 1
+    def is_route_bfs(self, start, end):
+        queue = [start] #pushing the start node to queue
+        visited = [start]
+        path = False
+
+        while queue:
+            denode = queue.pop(0) #pop first element
+
+            for adjnode in self.gdict[denode]:
+                if adjnode not in visited:
+                    if adjnode == end: #chehck the route
+                        path = True
+                        break
+                    else:
+                        visited.append(adjnode) #if not path the mark it as visited
+                        queue.append(adjnode)
+        return path
